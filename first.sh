79587b80c0796051eb8136ad1ef7784d0bfee81e
chandrakala <chadrakalasv1502@gmail.com>
2022-04-27 00:11:20 +0530

    [chandu]flip coin

--- first.sh
+++ first.sh
@@ -20,4 +20,17 @@ if [[ ${Result} -eq 1 ]];
         tails=$((tails + 1))
         echo $tails
 fi
+if [[ $tails -eq 21 ]]
+then
+	k=`expr $tails - $heads`
+	echo "tail won $k times"
+        exit 0
+fi
+if [[ $heads -eq 21 ]]
+then
+        k=`expr $heads - $tails`
+        echo "heads won $k times"
+        exit 0
+fi
+
 done
