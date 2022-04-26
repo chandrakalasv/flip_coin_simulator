1e0c91d36841b5b8019eb1daac7f943c6e4a9322
chandrakala <chadrakalasv1502@gmail.com>
2022-04-26 23:40:28 +0530

    [chandu]filp coin

---
+++ flipcoinsimulation.sh
@@ -0,0 +1,33 @@
+#! /bin/bash -x
+echo "flip coin simulation"
+tails=0
+heads=0
+Result=''
+while true
+do
+Result=$((RANDOM%2))
+if [[ ${Result} -eq 0 ]];
+        then
+        echo HEADS
+        heads=$((heads + 1))
+        echo $heads
+fi
+if [[ ${Result} -eq 1 ]];
+        then
+        echo TAILS
+        tails=$((tails + 1))
+        echo $tails
+fi
+if [[ $tails -eq 21 ]]
+then
+	k=`expr $tails - $heads`
+	echo "tail won $k times"
+        exit 0
+fi
+if [[ $heads -eq 21 ]]
+then
+        k=`expr $heads - $tails`
+        echo "heads won $k times"
+        exit 0
+fi
+done
