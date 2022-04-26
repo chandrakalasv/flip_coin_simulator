11f3dfcda47d27fffe62175c7a44c4caad7f616f
chandrakala <chadrakalasv1502@gmail.com>
2022-04-27 00:07:50 +0530

    [chandu]flip coin

--- first.sh
+++ first.sh
@@ -1,2 +1,20 @@
 #! /bin/bash/
 echo "filp coin simulation"
+
+tails=0
+heads=0
+Result=''
+Result=$((RANDOM%2))
+if [[ ${Result} -eq 0 ]];
+        then
+        echo HEADS
+        heads=$((heads + 1))
+        echo $heads
+fi
+
+if [[ ${Result} -eq 1 ]];
+        then
+        echo TAILS
+        tails=$((tails + 1))
+        echo $tails
+fi
